a0a2d00bac030032318584754e912bc3be864e97
vitor-amartins <vmam@cin.ufpe.br>
2024-08-05 17:30:06 -0300

    Gremlin promise with wrap future

--- app/queries.py
+++ app/queries.py
@@ -1,15 +1,15 @@
-import asyncio
+from asyncio import wrap_future
 
-from gremlin_python.process.graph_traversal import GraphTraversalSource, __
+from gremlin_python.process.graph_traversal import GraphTraversalSource, __, GraphTraversal
+
+
+async def async_next(t: GraphTraversal):
+    return await wrap_future(t.promise(lambda x: x.next()))
 
 
 async def count_vertex_by_label(g: GraphTraversalSource):
-    def func():
-        return g.V().groupCount().by(__.label()).next()
-    return await asyncio.to_thread(func)
+    return await async_next(g.V().groupCount().by(__.label()))
 
 
 async def count_edge_by_label(g: GraphTraversalSource):
-    def func():
-        return g.E().groupCount().by(__.label()).next()
-    return await asyncio.to_thread(func)
+    return await async_next(g.E().groupCount().by(__.label()))
